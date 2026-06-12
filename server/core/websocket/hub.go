469333494c1243a20462ff489017d7bc42597f21
jsndz <jaisondz9360@gmail.com>
2026-06-12 11:56:19 +0530

    websocket structs

--- server/core/websocket/hub.go
+++ server/core/websocket/hub.go
@@ -1,10 +1,47 @@
 package websocket
 
+import (
+	"errors"
+	"sync"
+)
+
 type Hub struct {
 	Clients map[string]*Client
 	Rooms   map[string]*Room
+	mu      sync.Mutex
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		Clients: make(map[string]*Client),
+		Rooms:   make(map[string]*Room),
+	}
 }
 
 func (h *Hub) Register(client *Client, id string) {
 	h.Clients[id] = client
+}
+
+func (h *Hub) CreateRoom() string {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	room := NewRoom()
+	h.Rooms[room.Id] = room
+	return room.Id
+}
+
+func (h *Hub) GetRoom(id string) (*Room, error) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	room, ok := h.Rooms[id]
+	if !ok {
+		return nil, errors.New("Room doesn't exist")
+	}
+	return room, nil
+}
+
+func (h *Hub) DeleteRoom(roomId string) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	delete(h.Rooms, roomId)
 }
--- server/core/websocket/room.go
+++ server/core/websocket/room.go
@@ -1,6 +1,44 @@
 package websocket
 
+import (
+	"lanos/pkg/id"
+	"sync"
+)
+
 type Room struct {
-	RoomId  string
+	Id      string
 	Clients map[string]*Client
+	mu      sync.Mutex
+}
+
+func NewRoom() *Room {
+	return &Room{
+		Id:      id.NewID(),
+		Clients: make(map[string]*Client),
+	}
+}
+
+func (r *Room) AddClient(c *Client) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.Clients[c.ID] = c
+}
+
+func (r *Room) RemoveClient(client *Client) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	delete(r.Clients, client.ID)
+}
+
+func (r *Room) GetAllClient() map[string]*Client {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	return r.Clients
+
+}
+
+func (r *Room) IsEmpty() bool {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	return len(r.Clients) == 0
 }
